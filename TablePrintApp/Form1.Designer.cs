982572405fc872972c2b3a1202710423e6f7184f
Admin <Admin@DESKTOP-8Q7I3CV>
2024-04-28 00:36:29 +0500

    Add project files.

---
+++ TablePrintApp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TablePrintApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTable = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtTable
+            // 
+            this.txtTable.AutoSize = true;
+            this.txtTable.Location = new System.Drawing.Point(46, 63);
+            this.txtTable.Name = "txtTable";
+            this.txtTable.Size = new System.Drawing.Size(102, 13);
+            this.txtTable.TabIndex = 0;
+            this.txtTable.Text = "Enter Table Number";
+            // 
+            // txtStart
+            // 
+            this.txtStart.AutoSize = true;
+            this.txtStart.Location = new System.Drawing.Point(46, 128);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(156, 13);
+            this.txtStart.TabIndex = 1;
+            this.txtStart.Text = "Enter  Starting Number of Table";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.AutoSize = true;
+            this.txtEnd.Location = new System.Drawing.Point(46, 199);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(136, 13);
+            this.txtEnd.TabIndex = 2;
+            this.txtEnd.Text = "Enter End of Table Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Print Table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 128);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(227, 196);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 29);
+            this.textBox3.TabIndex = 7;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(534, 63);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(223, 292);
+            this.txtResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.txtTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtTable;
+        private System.Windows.Forms.Label txtStart;
+        private System.Windows.Forms.Label txtEnd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
